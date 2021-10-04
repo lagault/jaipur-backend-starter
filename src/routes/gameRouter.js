5577689ec194789629e03e356297a0d91a2f7460
lagault <lagault@a112pc26L.local.isima.fr>
2021-10-04 19:39:42 +0200

    ajout des codes qui marchent

---
+++ src/routes/gameRouter.js
@@ -0,0 +1,13 @@
+import express from "express"
+import * as gameService from "../services/gameService"
+
+const router = express.Router()
+
+// Listen to POST /games
+router.post("/", function (req, res) {
+    if (!req.body.name) {
+      return res.status(400).send("Missing name parameter")
+    }
+    const newGame = gameService.createGame(req.body.name)
+    res.status(201).json(newGame)
+  })
