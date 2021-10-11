41c25299ca726110e924cba73a6631a7a24ff319
lagault <lagault@a112pc26L.local.isima.fr>
2021-10-11 19:52:15 +0200

    Ajout des codes corrigés (TP5)

--- src/routes/gameRouter.js
+++ src/routes/gameRouter.js
@@ -1,13 +1,17 @@
 import express from "express"
-import * as gameService from "../services/gameService"
+import * as gameServices from "../services/gameServices"
 
 const router = express.Router()
 
 // Listen to POST /games
 router.post("/", function (req, res) {
-    if (!req.body.name) {
-      return res.status(400).send("Missing name parameter")
-    }
-    const newGame = gameService.createGame(req.body.name)
-    res.status(201).json(newGame)
-  })
+  if (!req.body.name) {
+    return res.status(400).send("Missing name parameter")
+  }
+  const newGame = gameServices.createGame(req.body.name)
+  res.status(201).json(newGame)
+})
+
+export function Id(deck) {
+  return deck
+}
---
+++ src/routes/gameRouter.test.js
@@ -0,0 +1,93 @@
+import request from "supertest"
+import app from "../app"
+import lodash from "lodash"
+
+// Prevent database service to write tests game to filesystem
+jest.mock("fs")
+
+// Prevent shuffle for tests
+jest.mock("lodash")
+lodash.shuffle.mockImplementation((array) => array)
+
+describe("Game router", () => {
+  test("should create a game", async () => {
+    const expectedGame = {
+      id: 1,
+      name: "test",
+      market: ["camel", "camel", "camel", "diamonds", "diamonds"],
+      _deck: [
+        "silver",
+        "silver",
+        "silver",
+        "silver",
+        "silver",
+        "silver",
+        "cloth",
+        "cloth",
+        "cloth",
+        "cloth",
+        "cloth",
+        "cloth",
+        "cloth",
+        "cloth",
+        "spice",
+        "spice",
+        "spice",
+        "spice",
+        "spice",
+        "spice",
+        "spice",
+        "spice",
+        "leather",
+        "leather",
+        "leather",
+        "leather",
+        "leather",
+        "leather",
+        "leather",
+        "leather",
+        "leather",
+        "leather",
+        "camel",
+        "camel",
+        "camel",
+        "camel",
+        "camel",
+        "camel",
+        "camel",
+        "camel",
+      ],
+      _players: [
+        {
+          hand: ["diamonds", "diamonds", "diamonds", "diamonds", "gold"],
+          camelsCount: 0,
+          score: 0,
+        },
+        {
+          hand: ["gold", "gold", "gold", "gold", "gold"],
+          camelsCount: 0,
+          score: 0,
+        },
+      ],
+      currentPlayerIndex: 0,
+      tokens: {
+        diamonds: [7, 7, 5, 5, 5],
+        gold: [6, 6, 5, 5, 5],
+        silver: [5, 5, 5, 5, 5],
+        cloth: [5, 3, 3, 2, 2, 1, 1],
+        spice: [5, 3, 3, 2, 2, 1, 1],
+        leather: [4, 3, 2, 1, 1, 1, 1, 1, 1],
+      },
+      _bonusTokens: {
+        3: [2, 1, 2, 3, 1, 2, 3],
+        4: [4, 6, 6, 4, 5, 5],
+        5: [8, 10, 9, 8, 10],
+      },
+      isDone: false,
+    }
+
+    const response = await request(app).post("/games").send({ name: "test" })
+    expect(response.statusCode).toBe(201)
+    expect(response.body).toStrictEqual(expectedGame)
+  })
+})
